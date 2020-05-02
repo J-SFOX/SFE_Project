fb4ae4a438d1c6908ad35225a5e64b6c4adbaa9e
jemmalsoufiane@gmail.com <jemmalsoufiane@gmail.com>
2020-05-02 04:02:19 +0000

    adding the react-bootstrap things

--- Backend/src/sfe_elearning_project/settings.py
+++ Backend/src/sfe_elearning_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myApp',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'sfe_elearning_project.urls'
@@ -137,4 +140,4 @@ REST_FRAMEWORK = {
     ]
 }
 
-# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = True
--- Frontend/gui/src/App.js
+++ Frontend/gui/src/App.js
@@ -1,8 +1,50 @@
 import React from "react";
 import "./App.css";
+import Footer from "./Components/Footer";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+// import Container from "react-bootstrap/Container";
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Programmeur: "Soufiane Jemmal",
+      societe: "EST Essaouira",
+    };
+  }
+  render() {
+    return (
+      <Router>
+        <Container className="p-0" fluid={true}>
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>
+              <strong>EST</strong> Essaouira
+            </Navbar.Brand>
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav>
+                <Link className="nav-link" to="/">
+                  Accueil
+                </Link>
+                <Link className="nav-link" to="/">
+                  Etudier
+                </Link>
+                <Link className="nav-link" to="/">
+                  Information
+                </Link>
+                <Link className="nav-link" to="/">
+                  Contact
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+          <Footer />
+        </Container>
+      </Router>
+    );
+  }
 }
 
 export default App;
